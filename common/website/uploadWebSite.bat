d506159d17dd151bf915b83b1e4d2fe8f6d19a97
Daniel Gollás <gollas@jefecorp.com>
2012-10-18 19:59:56 -0500

    upload website

---
+++ common/website/uploadWebSite.bat
@@ -0,0 +1,17 @@
+copy ..\..\installers\*.* .\jefecheck\binaries\
+@echo off
+echo user jefeco5> ftpcmd.dat
+set choice=
+set /p choice=Enter password:
+echo %choice%>> ftpcmd.dat
+echo bin>> ftpcmd.dat
+echo cd www >> ftpcmd.dat
+echo cd jefecheck >> ftpcmd.dat
+echo mkdir binaries >> ftpcmd.dat
+echo cd binaries >> ftpcmd.dat
+echo mput C:\projects\jefecheck2\common\website\jefecheck\binaries\Jefe*.*>> ftpcmd.dat
+echo cd .. >> ftpcmd.dat
+echo mput C:\projects\jefecheck2\common\website\jefecheck\*.html>> ftpcmd.dat
+echo quit>> ftpcmd.dat
+ftp -n -i -s:ftpcmd.dat ftp.jefecorp.com
+REM del ftpcmd.dat
